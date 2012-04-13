ca7dcb879130ee9087a27cf5d0ac61b3ebbfecb9
liang shaowen <413472212@qq.com>
2012-04-13 11:37:42 +0800

    test round functions,just do the score and add pins

---
+++ bowlingGameScoring/BowlingGameRound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bowlingGameScoring
+{
+    public class BowlingGameRound
+    {
+        private int score;
+        public int  Score      
+        {
+            get { return score;  }
+        }
+        public void AddDownPins(int pins)
+        {
+            score += pins;
+        }
+
+       
+    }
+}
